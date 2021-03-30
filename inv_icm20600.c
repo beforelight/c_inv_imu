32f2d8b2daa22dbdd90062cefa4079be4d6bd4d5
beforelight <1761690868@qq.com>
2021-03-30 20:51:05 +0800

    add icm20600

---
+++ inv_icm20600.c
@@ -0,0 +1,48 @@
+﻿//
+// Created by 17616 on 2021/3/30.
+//
+
+#include "inv_icm20600.h"
+
+inv_imu_vector_table icm20600_VectorTable =
+        {
+                .Init = (void *) ICM20602_Init,
+                .Detect =(void *) ICM20600_Detect,
+                .SelfTest =(void *) ICM20602_SelfTest,
+                .Report =(void *) ICM20600_Report,
+                .DataReady =(void *) ICM20602_DataReady,
+                .EnableDataReadyInt =(void *) ICM20602_EnableDataReadyInt,
+                .SoftReset =(void *) ICM20602_SoftReset,
+                .ReadSensorBlocking =(void *) ICM20602_ReadSensorBlocking,
+                .ReadSensorNonBlocking =(void *) ICM20602_ReadSensorNonBlocking,
+                .Convert =(void *) ICM20602_Convert,
+                .Convert2 =(void *) ICM20602_Convert2,
+                .Convert3 =(void *) ICM20602_Convert3,
+                .IsOpen =(void *) _IMU_IsOpen
+        };
+
+inv_icm20600_handle ICM20600_Construct(inv_i2c _i2c, uint16_t _addr) {
+    inv_icm20600_handle rtv = (inv_icm20600_handle) ICM20602_Construct(_i2c, _addr);
+    rtv->parents.parents.vtable = &icm20600_VectorTable;
+    return rtv;
+}
+inv_icm20600_handle ICM20600_Construct2(inv_spi _spi) {
+    inv_icm20600_handle rtv = (inv_icm20600_handle) ICM20602_Construct2(_spi);
+    rtv->parents.parents.vtable = &icm20600_VectorTable;
+    return rtv;
+}
+bool ICM20600_Detect(inv_icm20602_handle this) {
+    uint8_t val = 0;
+    if (this->parents.addrAutoDetect) { this->parents.i2cTransfer.slaveAddress = 0x68; }
+    IMU_ReadReg((inv_imu_handle) this, (uint8_t) ICM20602_WHO_AM_I, &val);
+    if (0x11 == val) {
+        return true;
+    }
+    val = 0;
+    if (this->parents.addrAutoDetect) { this->parents.i2cTransfer.slaveAddress = 0x69; }
+    IMU_ReadReg((inv_imu_handle) this, (uint8_t) ICM20602_WHO_AM_I, &val);
+    if (0x11 == val) {
+        return true;
+    }
+    return false;
+}
---
+++ inv_icm20600.h
@@ -0,0 +1,25 @@
+﻿//
+// Created by 17616 on 2021/3/30.
+//
+
+#ifndef INV_IMU_INV_ICM20600_H
+#define INV_IMU_INV_ICM20600_H
+#include "inv_icm20602.h"
+
+typedef struct __inv_icm20600 {
+    inv_icm20602 parents;
+} inv_icm20600, *inv_icm20600_handle;
+
+
+
+void ICM20600_Destruct(inv_icm20600_handle this) { ICM20602_Destruct((void *) this); }
+inv_icm20600_handle ICM20600_Construct(inv_i2c _i2c, uint16_t _addr);
+inv_icm20600_handle ICM20600_Construct2(inv_spi _spi);
+
+
+
+bool ICM20600_Detect(inv_icm20602_handle this);
+const char *ICM20600_Report(inv_icm20602_handle this) { return " icm20600"; }
+
+
+#endif //INV_IMU_INV_ICM20600_H
