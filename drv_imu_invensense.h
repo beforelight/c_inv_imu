4ab46377d0502cea42a0162a34f8988a5f6d65c1
Chekhov Mark <maqike@qq.com>
2021-04-10 06:56:14 +0800

    新顶层框架

--- drv_imu_invensense.h
+++ drv_imu_invensense.h
@@ -1,4 +1,4 @@
-﻿#ifndef C_INV_IMU_INV_IMU_H
+#ifndef C_INV_IMU_INV_IMU_H
 #define C_INV_IMU_INV_IMU_H
 #include <inc_stdlib.h>
 #include <hitsic_common.h>
@@ -173,6 +173,228 @@ extern const struct _inv_weak_map_int ICM20948_GBW_MAP;
 //#if defined(__cplusplus) || defined(c_plusplus)
 //}
 //#endif
+
+#define kStatusGroup_INVIMU 300
+
+enum
+{
+    kStatus_INVIMU_DetectFail = MAKE_STATUS(kStatusGroup_INVIMU, 0),
+    kStatus_INVIMU_SelftestFail  = MAKE_STATUS(kStatusGroup_INVIMU, 1),
+};
+
+enum invimu_type_t 
+{
+    invimu_type_invalid = 0U,
+    invimu_type_mpu6050_i2c,
+    invimu_type_mpu9250_i2c,
+    invimu_type_mpu9250_spi,
+    invimu_type_icm20602_i2c,
+    invimu_type_icm20602_spi,
+    invimu_type_icm20948_i2c,
+    invimu_type_icm20948_spi,
+};
+
+enum invimu_pptFlag_t
+{
+    invimu_pptFlag_dataInit = 1U << 0U,
+    invimu_pptFlag_acceInit = 1U << 1U,
+    invimu_pptFlag_gyroInit = 1U << 2U,
+    invimu_pptFlag_magnInit = 1U << 3U,
+};
+
+typedef status_t(*invimu_spi_transfer_t)(uint8_t _regAddr, uint8_t *_data, uint32_t _size);
+
+typedef status_t(*invimu_i2c_transfer_t)(uint8_t _devAddr, uint8_t _regAddr, uint8_t *_data, uint32_t _size);
+
+typedef struct _invimu_spiBus
+{
+    invimu_spi_transfer_t rxBlocking;
+    invimu_spi_transfer_t txBlocking;
+}invimu_spiBus_t;
+
+typedef struct _invimu_i2cBus
+{
+    invimu_i2c_transfer_t rxBlocking;
+    invimu_i2c_transfer_t txBlocking;
+}invimu_i2cBus_t;
+
+typedef struct _invimu_config
+{
+    invimu_type_t type;
+    union
+    {
+        invimu_spiBus_t spiBus;
+        invimu_i2cBus_t i2cBus;
+    }busHandle;
+}invimu_config_t;
+
+typedef struct _invimu_acceConfig
+{
+
+}invimu_acceConfig_t;
+
+typedef struct _invimu_gyroConfig
+{
+
+}invimu_gyroConfig_t;
+
+typedef struct _invimu_magnConfig
+{
+
+}invimu_acceConfig_t;
+
+typedef struct _invimu_adapter invimu_adapter_t;
+
+typedef struct _invimu_i2cIfce
+{
+    invimu_type_t type;
+
+    uint32_t pptFlag;
+
+    union 
+    {
+        invimu_default_handle_t         default_handle;
+        invimu_mpu6050_i2c_handle_t*    mpu6050_i2c_handle;
+        invimu_mpu9250_i2c_handle_t*    mpu9250_i2c_handle;
+        invimu_icm20602_i2c_handle_t*   icm20602_i2c_handle;
+        invimu_icm20948_i2c_handle_t*   icm20948_i2c_handle;
+        invimu_mpu6000_spi_handle_t*    mpu6000_spi_handle;
+        invimu_mpu9250_spi_handle_t*    mpu9250_spi_handle;
+        invimu_icm20602_spi_handle_t*   icm20602_spi_handle;
+        invimu_icm20948_spi_handle_t*   icm20948_spi_handle;
+    } devHandle;
+
+    const invimu_adapter_t* adapter;
+}invimu_ifce_t;
+
+struct _invimu_adapter
+{
+    void (*SoftReset)(invimu_ifce_t *_inst);
+    status_t (*AcceInit)(invimu_ifce_t *_inst, invimu_acceConfig_t *_config);
+    status_t (*GyroInit)(invimu_ifce_t *_inst, invimu_gyroConfig_t *_config);
+    status_t (*MagnInit)(invimu_ifce_t *_inst, invimu_magnConfig_t *_config);
+
+    status_t (*Selftest)(invimu_ifce_t *_inst);
+
+    status_t (*AcceReadBlocking)(invimu_ifce_t *_inst);
+    status_t (*GyroReadBlocking)(invimu_ifce_t *_inst);
+    status_t (*MagnReadBlocking)(invimu_ifce_t *_inst);
+    status_t (*TempReadBlocking)(invimu_ifce_t *_inst);
+
+    void (*AcceConvertData)(invimu_ifce_t *_inst, float *data_x, float *data_y, float *data_z);
+    void (*GyroConvertData)(invimu_ifce_t *_inst, float *data_x, float *data_y, float *data_z);
+    void (*MagnConvertData)(invimu_ifce_t *_inst, float *data_x, float *data_y, float *data_z);
+    void (*TempConvertData)(invimu_ifce_t *_inst, float *data);
+};
+
+
+static inline status_t INVIMU_DataInit(invimu_ifce_t *_inst, invimu_config_t *_config)
+{
+    assert(_inst);
+    assert(_config);
+
+    _inst->pptFlag = 0U;
+    _inst->type = _config->type;
+
+    switch(_inst->type)
+    {
+    case invimu_type_mpu6050_i2c:
+        INVIMU_MPU6050_I2C_Construct(_inst->devHandle.mpu6050_i2c_handle, _config->busHandle.i2cBus);
+        _inst->adapter = invimu_mpu6050_i2c_adapter;
+        break;
+
+    //TODO: add other type here.
+
+    case invimu_type_invalid:
+    default:
+        _inst->type = invimu_type_invalid;
+        _inst->devHandle->default_handle = NULL;
+        _inst->adapter = NULL;
+        break;
+
+    }
+    
+}
+
+static inline void INVIMU_DataDeinit(invimu_ifce_t *_inst)
+{
+    _inst->pptFlag = 0U;
+    _inst->type = invimu_type_invalid;
+    free(_inst->devHandle->default_handle);
+    _inst->devHandle->default_handle = NULL;
+    _inst->adapter = NULL;
+    
+}
+
+static inline status_t INVIMU_SoftReset(invimu_ifce_t *_inst)
+{
+    _inst->adapter->SoftReset(_inst);
+}
+
+static inline status_t INVIMU_AcceInit(invimu_ifce_t *_inst, invimu_acceConfig_t *_config)
+{
+    _inst->adapter->AcceInit(_inst, _config);
+}
+static inline status_t INVIMU_GyroInit(invimu_ifce_t *_inst, invimu_gyroConfig_t *_config)
+{
+    _inst->adapter->GyroInit(_inst, _config);
+}
+static inline status_t INVIMU_MagnInit(invimu_ifce_t *_inst, invimu_magnConfig_t *_config)
+{
+    _inst->adapter->MagnInit(_inst, _config);
+}
+
+static inline status_t INVIMU_Selftest(invimu_ifce_t *_inst)
+{
+    _inst->adapter->Selftest(_inst);
+}
+
+static inline status_t INVIMU_AcceReadBlocking(invimu_ifce_t *_inst)
+{
+    _inst->adapter->AcceReadBlocking(_inst);
+}
+static inline status_t INVIMU_GyroReadBlocking(invimu_ifce_t *_inst)
+{
+    _inst->adapter->GyroReadBlocking(_inst);
+}
+static inline status_t INVIMU_MagnReadBlocking(invimu_ifce_t *_inst)
+{
+    _inst->adapter->MagnReadBlocking(_inst);
+}
+static inline status_t INVIMU_TempReadBlocking(invimu_ifce_t *_inst)
+{
+    _inst->adapter->TempReadBlocking(_inst);
+}
+
+static inline void INVIMU_AcceConvertData(invimu_ifce_t *_inst, float *data_x, float *data_y, float *data_z)
+{
+    _inst->adapter->AcceConvertData(_inst);
+}
+static inline void INVIMU_GyroConvertData(invimu_ifce_t *_inst, float *data_x, float *data_y, float *data_z)
+{
+    _inst->adapter->GyroConvertData(_inst);
+}
+static inline void INVIMU_MagnConvertData(invimu_ifce_t *_inst, float *data_x, float *data_y, float *data_z)
+{
+    _inst->adapter->MagnConvertData(_inst);
+}
+static inline void INVIMU_TempConvertData(invimu_ifce_t *_inst, float *data)
+{
+    _inst->adapter->TempConvertData(_inst);
+}
+
+
+
+/** MPU6050 */
+
+typedef struct invimu_mpu6050_handle_t
+{
+    
+};
+
+
+
+
 
 #endif //C_INV_IMU_INV_IMU_H
 
